54cb55a4721d7ca98ed545f7d5a02e59c0108f9a
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-08-28 22:17:31 +0100

    Lançamento do projecto

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function desb() {
+    let nome = document.querySelector('input#tname').value.trim();
+
+    // Nome completo esperado
+    const nomeCompleto = 'Dulce Sumluisse Martelo';
+
+    // Verifica se o campo está vazio
+    if (nome === '') {
+        alert('[ERRO] Campo vazio, por favor! verifique e tente novamente.');
+    }
+    // Verifica se o nome inserido tem menos de três palavras
+    else if (nome.split(' ').length < 3) {
+        alert('[ERRO] Por favor! digite seu nome completo, igual ao do BI (bilhete de identidade).');
+    }
+    // Verifica se o nome não corresponde ao nome completo
+    else if (nome !== nomeCompleto) {
+        alert('[ERRO] Nome inválido, desculpa essa surpresa não é para ti 😿');
+    }
+    // Nome completo correto
+    else {
+        alert(`Parabéns! 😊🎁 🔹${nome}🔹, você será redirecionada para a sua surpresa 😉`);
+        // Redirecionar para a página de surpresa
+        redirectToSurprise(nome);
+    }
+
+    // Limpa o campo após a tentativa
+    document.querySelector('input#tname').value = '';
+}
+
+function redirectToSurprise(nome) {
+    if (nome) {
+        // Redirecionar para a página de surpresa com o nome como parâmetro na URL
+        window.location.href = `surprise.html?name=${encodeURIComponent(nome)}`;
+    }
+}
